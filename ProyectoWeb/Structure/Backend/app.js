e320a8b3e2bacdbbbcde6523588de7f781a80daa
XarlosZakk <carlosgamer1009@gmail.com>
2025-05-28 13:43:22 -0500

    cambios Nuevos

---
+++ ProyectoWeb/Structure/Backend/app.js
@@ -0,0 +1,11 @@
+// let nuevoArray = ['camilo', 'juan', 'miguel', 'ramires'];
+// let newObj =  {
+//     nombre: 'Ernesto',
+//     apellido: 'gonzales',
+//     direccion: 'Avenida 24 de febrero calle 24No',
+//     telefono: "0963567721",
+//     edad: 22,
+//     Genero: 'Hombre - Masculino'
+// } 
+
+// console.log(newObj);
--- ProyectoWeb/Structure/Backend/appEmp.js
+++
@@ -1,7 +0,0 @@
-//! DOM de la pagina web Empresarial 
-
-//capturando el elemento
-let valorSelector = document.getElementById("baner");
-console.log(valorSelector);
-console.log(valorSelector);
-
